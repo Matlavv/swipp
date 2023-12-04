7832b11a786b03ea89b9feaaad95ce1fdcb9d01e
matlavv <mathis.laversin92@gmail.com>
2023-12-04 16:47:01 +0100

    Bottom navbar

--- App.js
+++ App.js
@@ -1,36 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
 
-export default function App() {
-  const Stack = createNativeStackNavigator();
 
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{
-                headerShown: false,
-              }}
-            />
-          </Stack.Navigator>
-        </SafeAreaProvider>
-      </NavigationContainer>
-    </Provider>
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const ProfileScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Profil !</Text>
+  </View>
+);
+
+const ServiceScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Services !</Text>
+  </View>
+);
+
+const HistoryScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>History !</Text>
+  </View>
+);
+
+  const HomeStack = () => (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+  export default function App() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: 'black',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false
+          }}
+        >
+          <Tab.Screen
+            name="Accueil"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Services "
+            component={ServiceScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="apps" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Historique"
+            component={HistoryScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="archive-outline" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profil"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }

52e46bd392e6511f0f9c09bc13a8a043adbadcf0
matlavv <mathis.laversin92@gmail.com>
2023-12-05 12:09:07 +0100

    Navigation between pages & Profile page

--- App.js
+++ App.js
@@ -1,92 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
-import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { store } from './store';
-import HomeScreen from './screens/HomeScreen';
 
+import HomeScreen from './screens/HomeScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ServiceScreen from './screens/ServiceScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const ProfileScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Profil !</Text>
-  </View>
-);
 
-const ServiceScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Services !</Text>
-  </View>
-);
-
-const HistoryScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>History !</Text>
-  </View>
-);
-
-  const HomeStack = () => (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
-  
-  export default function App() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarActiveTintColor: 'black',
-            tabBarInactiveTintColor: 'gray',
-            headerShown: false
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="Accueil"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
           }}
-        >
-          <Tab.Screen
-            name="Accueil"
-            component={HomeStack}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="home" size={size} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Services "
-            component={ServiceScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="apps" size={size} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Historique"
-            component={HistoryScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="archive-outline" size={size} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Profil"
-            component={ProfileScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="person" size={size} color={color} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
+        />
+        <Tab.Screen
+          name="Services"
+          component={ServiceScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="apps" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Historique"
+          component={HistoryScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="archive-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profil"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

e61df33acfd9883d94990113a0c30201eb02fa5a
matlavv <mathis.laversin92@gmail.com>
2024-01-25 16:44:43 +0100

    Choose date on garage form

--- components/Forms/GarageList.jsx
+++ components/Forms/GarageList.jsx
@@ -1,10 +1,29 @@
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 import { garage1 } from "../../assets";
+import DateTimePickerModal from "./DateTimePickerModal";
 
 const GarageList = () => {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const [selectedGarage, setSelectedGarage] = useState(null);
+
+  const handleBookPress = (garage) => {
+    setSelectedGarage(garage);
+    setPickerVisible(true);
+  };
+
+  const handleClose = () => {
+    setPickerVisible(false);
+  };
+
+  const handleConfirm = (dateTime) => {
+    console.log("Réservation pour", selectedGarage, "à", dateTime);
+    // Ici, vous pouvez gérer la confirmation, comme la mise à jour d'un état ou l'envoi à un serveur
+    setPickerVisible(false);
+  };
+
   const garages = [
     {
       name: "ChrisFix Garage",
@@ -27,41 +46,49 @@ const GarageList = () => {
   ];
 
   return (
-    <ScrollView>
-      {garages.map((garage, index) => (
-        <View key={index} style={tw`flex w-95 ml-4`}>
-          <View
-            style={tw`flex mb-4 bg-white rounded-xl border border-gray-200 shadow-xl`}
-          >
-            <Image source={garage.image} style={tw`h-32 w-full rounded-t-xl`} />
-            <View style={tw`px-4 py-2 bg-white rounded-b-xl`}>
-              <Text style={tw`text-lg font-bold`}>{garage.name}</Text>
-              <View style={tw`flex-row justify-between pt-2`}>
-                <Text
-                  style={tw`text-sm text-black border-2 rounded-full px-3 py-1 border-slate-400`}
-                >
-                  {garage.description}
-                </Text>
-                <Text
-                  style={tw`text-sm text-black border-2 rounded-full px-3 py-1 border-slate-400`}
-                >
-                  {garage.workerCount} Workers
-                </Text>
-                <TouchableOpacity
-                  onPress={() => {
-                    console.log("Booking garage:", garage.name);
-                  }}
-                  style={tw`bg-[#34469C] px-4 py-1 rounded-full flex-row`}
-                >
-                  <Text style={tw`text-white text-sm mr-1`}>Book</Text>
-                  <Ionicons name="arrow-forward" size={20} color="white" />
-                </TouchableOpacity>
+    <>
+      <ScrollView>
+        {garages.map((garage, index) => (
+          <View key={index} style={tw`flex w-95 ml-4`}>
+            <View
+              style={tw`flex mb-4 bg-white rounded-xl border border-gray-200 shadow-xl`}
+            >
+              <Image
+                source={garage.image}
+                style={tw`h-32 w-full rounded-t-xl`}
+              />
+              <View style={tw`px-4 py-2 bg-white rounded-b-xl`}>
+                <Text style={tw`text-lg font-bold`}>{garage.name}</Text>
+                <View style={tw`flex-row justify-between pt-2`}>
+                  <Text
+                    style={tw`text-sm text-black border-2 rounded-full px-3 py-1 border-slate-400`}
+                  >
+                    {garage.description}
+                  </Text>
+                  <Text
+                    style={tw`text-sm text-black border-2 rounded-full px-3 py-1 border-slate-400`}
+                  >
+                    {garage.workerCount} Workers
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => handleBookPress(garage)}
+                    style={tw`bg-[#34469C] px-4 py-1 rounded-full flex-row`}
+                  >
+                    <Text style={tw`text-white text-sm mr-1`}>Book</Text>
+                    <Ionicons name="arrow-forward" size={20} color="white" />
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      ))}
-    </ScrollView>
+        ))}
+      </ScrollView>
+      <DateTimePickerModal
+        isVisible={isPickerVisible}
+        onClose={handleClose}
+        onConfirm={handleConfirm}
+      />
+    </>
   );
 };
 
